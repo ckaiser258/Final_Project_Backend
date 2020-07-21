2bc08e63240a503e394db2bdb7d16ccd96fa00dd
Colton <kaiserc2@seattleu.edu>
2020-07-21 14:28:55 -0700

    Uncomments has_many stats in team/athlete models

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,7 @@ class Team < ApplicationRecord
   belongs_to :user
   has_many :athletes
   has_many :injuries, through: :athletes
-  # has_many :stats, through: :athletes
+  has_many :stats, through: :athletes
 
   validates :name, presence: { message: "must be given" }
 end
