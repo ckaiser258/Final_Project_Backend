074cb856a7b03fbce2948dd45b2d8fcbb2243906
Colton <kaiserc2@seattleu.edu>
2020-07-30 09:12:04 -0700

    Permits id for user controller (may not be needed)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -33,6 +33,6 @@ class Api::V1::UsersController < ApplicationController
     private 
     
     def user_params
-        params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
+        params.require(:user).permit(:id, :first_name, :last_name, :username, :email, :password)
     end
 end
